555d9d307daf3b0dc6d72d916a115cd8a5f6eafb
Eduardo <euodeionomedeusuario@gmail.com>
2018-04-12 18:47:37 -0300

    fixing layout

--- classroom/controllers/ClassController.py
+++ classroom/controllers/ClassController.py
@@ -54,6 +54,33 @@ def send_invite(class_id):
     return "OK"
 
 
+def getMes(mes):
+    if mes == 1:
+        return "Janeiro"
+    if mes == 2:
+        return "Fevereiro"
+    if mes == 3:
+        return "Março"
+    if mes == 4:
+        return "Abril"
+    if mes == 5:
+        return "Maio"
+    if mes == 6:
+        return "Junho"
+    if mes == 7:
+        return "Julho"
+    if mes == 8:
+        return "Agosto"
+    if mes == 9:
+        return "Setembro"
+    if mes == 10:
+        return "Outubro"
+    if mes == 11:
+        return "Novembro"
+    if mes == 12:
+        return "Dezembro"
+
+
 @app.route("/classroom/user/<user_id>/classes/<class_id>/", methods=["GET"])
 def get_index_student(class_id, user_id):
     date = datetime.datetime.now().strftime("%Y-%m-%d")
