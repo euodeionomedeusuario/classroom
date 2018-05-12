37b76d1cbc593a150071de41832229f3d554e903
Eduardo <euodeionomedeusuario@gmail.com>
2018-05-12 15:14:05 -0300

    adding auth email

--- classroom/static/js/users.js
+++ classroom/static/js/users.js
@@ -14,22 +14,34 @@ $(document).ready(function(){
   /*Inicializando o botão collapse*/
   $(".button-collapse").sideNav();
 
-  /*atualizando informações pessoais do usuário*/
-  $("#btn-update-user").click(function(event){
-    var userId = $("#user-id").val();
+  $("#newPassword2").on("change", function(event) {
+    var $p1 = $("#newPassword");
+
+    if($p1.val() == $(this).val()) {
+      $(this).css("border-bottom", "1px solid green");
+    } else {
+      $(this).css("border-bottom", "1px solid red");
+    }
+  });
+
+
+  $("#btnEditPI").click(function(event){
+    var userId = $("#userId").val();
     var name = $("#name").val();
     var email = $("#email").val();
 
     $.ajax({
-      url: PROTOCOL + HOSTNAME + "/classroom/users/" + userId + "/",
+      url: URL + "/classroom/users/" + userId + "/",
       type: "PUT",
       data: {"name": name, "email": email},
       success: function(data) {
-        window.location.replace(PROTOCOL + HOSTNAME + "/classroom/");
-
+        window.location.replace(URL + "/classroom/");
+      },
+      error: function(data) {
+        $("#email-error").css("display", "block").css("color", "red").text(data["responseText"]);
       }
     });
-  }); /*fim*/
+  });
 
 
 });
