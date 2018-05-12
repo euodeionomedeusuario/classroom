ccc484400ece3cc055001eeb8c820d473f150cdc
Eduardo <euodeionomedeusuario@gmail.com>
2018-05-12 20:52:39 -0300

    authenticating to register users

--- classroom/static/js/signup.js
+++ classroom/static/js/signup.js
@@ -14,4 +14,18 @@ $(document).ready(function(){
   /* Inicialzando collapse button */
   $(".button-collapse").sideNav();
 
+  $("#newPassword2").on("change", function(event) {
+    var $p1 = $("#newPassword");
+
+    if($p1.val() == $(this).val()) {
+      $(this).css("border-bottom", "2px solid green");
+      $("#password-error").css("font-weight", "bold").css("display", "block").css("color", "green").text("Tudo bem. Senhas combinam!")
+
+    } else {
+      $(this).css("border-bottom", "2px solid red");
+      $("#password-error").css("font-weight", "bold").css("display", "block").css("color", "red").text("Ops! Senhas não combinam!")
+
+    }
+  });
+
 });
